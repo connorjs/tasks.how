117729a876c7173dbaaeea7f1816d2ef27adbdd9
Connor Sullivan <dev.connorjs@gmail.com>
2025-12-19 18:44:05 -0500

    ⬆️ Update dependencies
    Updates all dependencies. Uses .NET 10 stable release. Fixes artifacts
    output directory.

--- common/hosting.api/extensions/ConfigurationExtensions.cs
+++ common/hosting.api/extensions/ConfigurationExtensions.cs
@@ -11,7 +11,8 @@ internal static class ConfigurationExtensions
 	internal static T GetRequired<T>(this IConfiguration config, string sectionName)
 		where T : class
 	{
-		return config.GetSection(sectionName).Get<T>() ?? throw new InvalidOperationException(
+		return config.GetSection(sectionName).Get<T>()
+			?? throw new InvalidOperationException(
 				$"Missing required configuration section: {sectionName}"
 			);
 	}
