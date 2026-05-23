6d5ac47ecfb394a55fe707a35231d4f10a10b9c4
Connor Sullivan <dev.connorjs@gmail.com>
2026-05-23 12:46:46 -0400

    tsr, more tanstack start progress

--- example/web/router/router.js
+++ example/web/router/router.js
@@ -1,5 +1,5 @@
 import { createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { routeTree } from '../route_tree/route_tree.gen'
 
 export function getRouter() {
 	return createRouter({
