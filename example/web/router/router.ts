2dda75643139041cdf5a21de5a56a4ef1b257f62
Connor Sullivan <dev.connorjs@gmail.com>
2026-05-23 18:43:52 -0400

    Fix Gazelle (but now ts_project is broken)

--- example/web/router/router.ts
+++ example/web/router/router.ts
@@ -1,5 +1,5 @@
 import { createRouter } from '@tanstack/react-router'
-import { routeTree } from '../route_tree/route_tree.gen.ts'
+import { routeTree } from '../route_tree/route_tree.gen.js'
 
 export function getRouter() {
 	return createRouter({
