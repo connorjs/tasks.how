6bc56b32a18de6caa66bdf87dde4871984085646
Connor Sullivan <dev.connorjs@gmail.com>
2026-05-24 08:44:17 -0400

    vite builds NOT with bazel

--- example/web/vite.config.ts
+++ example/web/vite.config.ts
@@ -5,9 +5,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+	cacheDir: ".cache/vite",
 	plugins: [tanstackStart({
 		srcDirectory: ".",
 		router: {
+			routeFileIgnorePattern: "\\.gen\\.",
 			entry: "router/router.ts",
 			routesDirectory: "routes",
 			generatedRouteTree: "routes/route_tree.gen.ts",
