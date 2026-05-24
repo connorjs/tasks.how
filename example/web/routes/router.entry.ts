0bb5772ead4bf5615ee0e4eb6f285057eba0c5ec
Connor Sullivan <dev.connorjs@gmail.com>
2026-05-24 17:26:07 -0400

    Changes from review

--- example/web/routes/router.entry.ts
+++ example/web/routes/router.entry.ts
@@ -1,5 +1,5 @@
 import { createRouter } from "@tanstack/react-router";
-import { routeTree } from "./route_tree.gen.js";
+import { routeTree } from "./route_tree.gen";
 
 export function getRouter() {
 	return createRouter({
